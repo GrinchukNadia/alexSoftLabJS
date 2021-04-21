2c1dc0fa3817eb2cbfd08bd26530a1509123f6ab
Надежда <grennad991@gmail.com>
2021-04-21 12:53:19 +0300

    Created simple app which works with json object locall

---
+++ README.md
@@ -0,0 +1,8 @@
+# AlexSoftLab JS
+> The first step for the Front end course provided by Lab AltexSoft.
+
+Test task from two parts.This is the second part. 
+
+Need to generate an array of random unique letters and insert them to the select element as options. When the user selects an option, show a list of names from the list.json file where first letter matches with selected option.
+
+![image info](./img/app.jpg)
---
+++ js/script.js
@@ -0,0 +1,47 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const arr = []
+const elementSelect = document.getElementById('random_letters')
+const divWithResult = document.querySelector('.list_name')
+
+//Generate an array of random unique letters with a length equal to 5
+for(let i = 0; i<5; i++) {
+    let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    
+    if(!arr.includes(randomLetter)) {
+        arr.push(randomLetter)
+    } else {
+        i--
+    }
+}
+
+//Inserts <option> elements with the value of random letters from the array
+for(let one of arr) {
+    elementSelect.innerHTML += `
+        <option value="letter ${one.toUpperCase()}">${one.toUpperCase()}</option>
+    `
+}
+
+//Generating result of matches from JSON object
+fetch("../list.json")
+.then(res => res.json())
+.then(data => {
+    elementSelect.addEventListener('change', () => {
+        let valueElementSelect = elementSelect.options[elementSelect.selectedIndex].text;
+        divWithResult.innerHTML = '';
+
+        for(let dataItem of data) {
+            let nameItem = dataItem.name;
+            if(valueElementSelect === dataItem.name.slice(0, 1)) {
+                divWithResult.innerHTML += `<div class="name_item">${nameItem}</div>`
+            }
+        }
+        if(divWithResult.innerHTML == "") {
+            divWithResult.innerHTML = `No matches on letter ${valueElementSelect}`
+        }
+    })
+}).catch(() => {
+    divWithResult.innerHTML = `
+        <div>Oops!...</div>
+        <div>Something went wrong (⌣_⌣")</div>
+    `;
+  });
